f1ddcbdca6102aca818bdba9a7c8fd07addc67da
tanner-burkin <43300807+tanner-burkin@users.noreply.github.com>
2019-12-05 21:07:07 -0600

    Added city searching functionality.

--- sprint4/sprint3-searching.js
+++ sprint4/sprint3-searching.js
@@ -1,5 +1,17 @@
 function searchCity(){
-    document.getElementById("cityArea");
+    city = document.getElementById("cityArea").value;
+    geocoder = new google.maps.Geocoder();
+    console.log(city);
+    geocoder.geocode( { 'address': city}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        //Got result, center the map and put it out there
+        map.setCenter(results[0].geometry.location);
+        map.setZoom(6);
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+    return map;
 }
 
 function codeAddress(map) {
